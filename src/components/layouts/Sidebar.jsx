6ee58bb2d0c173869128587d54b1359747f48502
ctschnurr <ctschnurr@hotmail.com>
2025-02-20 15:12:42 -0400

    followed along in class Feb 20

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -13,7 +13,7 @@ import {
 
 const SidebarItem = ({ href, label, isCollapsed, icon }) => {
   const location = useLocation();
-  const isActive = location.pathname === href;
+  const isActive = location.pathname.startsWith(href);
 
   return (
     <li className="group">
